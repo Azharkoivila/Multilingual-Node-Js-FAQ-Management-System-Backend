d211d8b7e20ef66c0c5ac851efa7c6e73ba1353a
AzharKoivila <azharkoivila@gmail.com>
2025-02-03 12:15:40 +0530

    unwanted variables removed

--- routes/createFAQ.js
+++ routes/createFAQ.js
@@ -11,12 +11,11 @@ router.post('/create',function(req, res, next) {
   const {question,answer}=req.body;
   if(question && answer){
     let faq=new FAQSchema(question,answer);
-  const save=dbfunctions.saveFAQ(faq);
-  const tr=faq.transalate();
-  Promise.allSettled([save,tr]).then((e)=>{
-    res.send(e)
+  Promise.allSettled([dbfunctions.saveFAQ(faq),faq.transalate()]).then((e)=>{
+    res.send(e);
   }).catch((err)=>{
-    res.send(err)
+    res.status(500);
+    res.send(err);
   });
   }else{
     res.statusMessage = "Qustion and Answer Must Needed On Body";
